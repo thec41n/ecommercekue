6bfd372d776f61bb8bcbd6ec4faba8ce1ad55c60
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-05-08 22:42:24 +0700

    login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,36 +1,60 @@
-import User from '../models/userModel.js';
-import asyncHandler from '../middlewares/asyncHandler.js';
+import User from "../models/userModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body
-    
-    if (!username || !email || !password) {
-        throw new Error("Tolong isi semua input!");
-    }
+  const { username, email, password } = req.body;
 
-    const userExists = await User.findOne({ email })
-    if (userExists) res.status(400).send("Email yang digunakan sudah ada!");
+  if (!username || !email || !password) {
+    throw new Error("Tolong isi semua input!");
+  }
 
-    const salt = await bcrypt.genSalt(15)
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const newUser = new User({ username, email, password: hashedPassword });
+  const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).send("Email yang digunakan sudah ada!");
 
-    try {
-        await newUser.save();
+  const salt = await bcrypt.genSalt(15);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
 
-        res
-        .status(201)
-        .json({
-            _id: newUser._id, 
-            username: newUser.username, 
-            email: newUser.email, 
-            isAdmin: newUser.isAdmin,
-        });
-    } catch (error) {
-        res.status(400)
-        throw new Error("Data user tidak valid")
-    }
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Data user tidak valid");
+  }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password, existingUser.password
+    );
+
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
+      return;
+    }
+  }
+});
+
+export { createUser, loginUser };
