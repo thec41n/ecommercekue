b83b4176fd3f803d8e6defe7eaa972f18b7999c2
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-05-10 18:04:20 +0700

    feat: delete user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 import createToken from "../utils/createToken.js";
 
 // Create User
-const createUser = asyncHandler(async(req, res) => {
+const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
@@ -35,86 +35,118 @@ const createUser = asyncHandler(async(req, res) => {
 });
 
 // Login User
-const loginUser = asyncHandler(async(req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const existingUser = await User.findOne({ email });
 
-  if (existingUser) {
-    const isPasswordValid = await bcrypt.compare(
-      password, existingUser.password
-    );
+  if (!existingUser) {
+    res.status(404).json({ message: "Username atau Password Salah!" });
+    return;
+  }
 
-    if (isPasswordValid) {
-      createToken(res, existingUser._id);
+  const isPasswordValid = await bcrypt.compare(
+    password,
+    existingUser.password
+  );
 
-      res.status(201).json({
-        _id: existingUser._id,
-        username: existingUser.username,
-        email: existingUser.email,
-        isAdmin: existingUser.isAdmin,
-      });
-      return;
-    }
+  if (isPasswordValid) {
+    createToken(res, existingUser._id);
+
+    res.status(201).json({
+      _id: existingUser._id,
+      username: existingUser.username,
+      email: existingUser.email,
+      isAdmin: existingUser.isAdmin,
+    });
+  } else {
+    res.status(404).json({ message: "Username atau Password Salah!" });
   }
 });
 
 // Logout User
-const logoutCurrentUser = asyncHandler(async(req, res) => {
+const logoutCurrentUser = asyncHandler(async (req, res) => {
   res.cookie("jwt", "", {
     httpOnly: true,
     expires: new Date(0),
   });
 
-  res.status(200).json({message: "Berhasil Log out"});
+  res.status(200).json({ message: "Berhasil Log out" });
 });
 
 // Get All Users
-const getAllUsers = asyncHandler(async(req, res) => {
-  const users = await User.find({})
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
   res.json(users);
 });
 
 // Get Spesific User
-const getCurrentUserProfile = asyncHandler(async(req, res) => {
-  const user = await User.findById(req.user._id)
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
   if (user) {
     res.json({
       _id: user._id,
       username: user.username,
-      email: user.email
-    })
+      email: user.email,
+    });
   } else {
-    res.status(401)
+    res.status(401);
     throw new Error("User tidak ditemukan");
   }
-})
+});
 
 // Update User Profile
-const updateCurrentUserProfile = asyncHandler(async(req, res) => {
-  const user = await User.findById(req.user._id)
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
   if (user) {
-    user.username = req.body.username || user.username
-    user.email = req.body.email || user.email
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
 
     if (req.body.password) {
-      user.password = req.body.password
+      const salt = await bcrypt.genSalt(15);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
     }
 
     const updatedUser = await user.save();
 
-    res.json ({
+    res.json({
       _id: updatedUser._id,
       username: updatedUser.username,
       email: updatedUser.email,
       isAdmin: updatedUser.isAdmin,
     });
   } else {
-    res.status(404)
+    res.status(404);
     throw new Error("User tidak ditemukan!");
+  }
+});
+
+const deleteUserById = asyncHandler(async(req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error('Tidak Bisa Menghapus Admin!');
+    }
+
+    await User.deleteOne({_id: user._id});
+    res.json({message: "User berhasil dihapus"});
+  } else {
+    res.status(404);
+    throw new Error("User tidak ditemukan");
   }
 })
 
-export { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile };
+export {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,15 +6,25 @@ import {
   getAllUsers,
   getCurrentUserProfile,
   updateCurrentUserProfile,
+  deleteUserById,
 } from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
-router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
+
+// ADMIN ROUTES
+router.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById);
 
 export default router;
