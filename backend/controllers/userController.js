3581084bdbd570304607013a57c4ecbb70c9bcbb
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-05-10 17:20:06 +0700

    feat: update user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,12 +67,54 @@ const logoutCurrentUser = asyncHandler(async(req, res) => {
   });
 
   res.status(200).json({message: "Berhasil Log out"});
-})
+});
 
 // Get All Users
 const getAllUsers = asyncHandler(async(req, res) => {
   const users = await User.find({})
   res.json(users);
+});
+
+// Get Spesific User
+const getCurrentUserProfile = asyncHandler(async(req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email
+    })
+  } else {
+    res.status(401)
+    throw new Error("User tidak ditemukan");
+  }
 })
 
-export { createUser, loginUser, logoutCurrentUser, getAllUsers };
+// Update User Profile
+const updateCurrentUserProfile = asyncHandler(async(req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.username = req.body.username || user.username
+    user.email = req.body.email || user.email
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save();
+
+    res.json ({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404)
+    throw new Error("User tidak ditemukan!");
+  }
+})
+
+export { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,8 @@ import {
   loginUser,
   logoutCurrentUser,
   getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
 } from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
@@ -13,6 +15,6 @@ router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
-
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
 export default router;
