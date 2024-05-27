1531f76eaf2ac98dbd6a9e9bf425c97cf5d58176
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-05-27 20:57:41 +0700

    feat: login form

---
+++ frontend/src/pages/Auth/Login.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersApiSlice";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  return (
+    <div>
+      <section className="pl-[10rem] flex flex-wrap">
+        <div className="mr-[4rem] mt-[5rem]">
+          <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
+
+          <form className="container w-[40rem]">
+            <div className="my-[2rem]">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-black"
+              >
+                Email Address
+              </label>
+
+              <input
+                type="email"
+                id="email"
+                className="mt-1 p-2 border rounded w-full"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="my-[2rem]">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-black"
+              >
+                Password
+              </label>
+
+              <input
+                type="password"
+                id="password"
+                className="mt-1 p-2 border rounded w-full"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
