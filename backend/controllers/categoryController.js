af2e5d565e327aef36305faacc75c8ace8692bf6
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-06-13 16:05:45 +0700

    feat: category CRUD

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -24,30 +24,60 @@ const createCategory = asyncHandler(async (req, res) => {
 });
 
 const updateCategory = asyncHandler(async (req, res) => {
-    try {
-      const { name } = req.body;
-      const { categoryId } = req.params;
-  
-      const category = await Category.findOne({ _id: categoryId });
-  
-      if (!category) {
-        return res.status(404).json({ error: "Kategori tidak ditemukan" });
-      }
-  
-      category.name = name;
-  
-      const updatedCategory = await category.save();
-      res.json(updatedCategory);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal server error" });
+  try {
+    const { name } = req.body;
+    const { categoryId } = req.params;
+
+    const category = await Category.findOne({ _id: categoryId });
+
+    if (!category) {
+      return res.status(404).json({ error: "Kategori tidak ditemukan" });
     }
-  });
+
+    category.name = name;
+
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  try {
+    const deleted = await Category.findByIdAndDelete(req.params.categoryId);
+    res.json({ message: "Kategori berhasil dihapus", deleted });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const listCategory = asyncHandler(async (req, res) => {
+  try {
+    const all = await Category.find({});
+    res.json(all);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
+  }
+});
+
+const readCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.findOne({ _id: req.params.id });
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
+  }
+});
 
 export {
   createCategory,
   updateCategory,
-//   removeCategory,
-//   listCategory,
-//   readCategory,
+  deleteCategory,
+  listCategory,
+  readCategory,
 };
