9990116ded1ec5b6df69716b5be4dbca09424b93
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-06-13 17:06:43 +0700

    feat: category frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,7 @@ import Profile from "./pages/User/Profile.jsx";
 
 import AdminRoute from "./pages/Admin/AdminRoute";
 import UserList from "./pages/Admin/UserList";
+import CategoryList from "./pages/Admin/CategoryList";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -31,6 +32,7 @@ const router = createBrowserRouter(
       {/* Admin Routes */}
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
       </Route>
     </Route>
   )
