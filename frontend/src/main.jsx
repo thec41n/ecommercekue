cf749d9507ad4cb74a36fd5f36e90779c9cb48b8
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-06-18 15:19:02 +0700

    feat: product frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -19,6 +19,7 @@ import Profile from "./pages/User/Profile.jsx";
 import AdminRoute from "./pages/Admin/AdminRoute";
 import UserList from "./pages/Admin/UserList";
 import CategoryList from "./pages/Admin/CategoryList";
+import ProductList from "./pages/Admin/ProductList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,6 +34,7 @@ const router = createBrowserRouter(
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
         <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
       </Route>
     </Route>
   )
