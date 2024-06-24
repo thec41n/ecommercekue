0333d5982e89e66cc208460d9d7c860f5a0e6d72
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-06-24 14:50:46 +0700

    feat: product tabs, product details

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -141,7 +141,7 @@ const Navigation = () => {
         {dropdownOpen && userInfo && (
           <ul
             className={`absolute right-0 mt-2 mr-14 space-y-2 bg-white text-gray-600 ${
-              !userInfo.isAdmin ? "-top-20" : "-top-80 -mt-5"
+              !userInfo.isAdmin ? "-top-20" : "-top-80 -mt-7"
             } `}
             onClick={(e) => e.stopPropagation()}
           >
