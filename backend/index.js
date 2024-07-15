cc17457063b3685e68ab8bc7c52be5b0929babd8
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-07-15 10:43:23 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -29,9 +29,6 @@ app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
-// app.get("/api/config/paypal", (req, res) => {
-//   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
-// });
 
 app.get("/api/config/midtrans", (req, res) => {
   res.send({
